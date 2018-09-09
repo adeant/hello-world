ecd113b835146b87c7aeecdf295081829ff337f6
adeant <35200053+adeant@users.noreply.github.com>
2018-09-09 11:00:26 -0500

    Style updates

--- AtomPython/MathPython/scratch2.py
+++ AtomPython/MathPython/scratch2.py
@@ -1,17 +1,17 @@
-#import the regex module
+# import the regex module
 import re
-#set up the phone number regex
+# set up the phone number regex
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-#set the message
+# set the message
 message = 'My number is 415-555-4242.'
-#search for the match
+# search for the match
 mo = phoneNumRegex.search(message)
-#print the output
-#print(mo.group())
+# print the output
+# print(mo.group())
 
-#set up the phone number regex
+# set up the phone number regex
 phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-#search for the match
+# search for the match
 mo = phoneNumRegex.search(message)
-#print the output
+# print the output
 print(mo.group(2))
