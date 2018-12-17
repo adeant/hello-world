797eeeba08735f139134f698b61dd35867d6fac9
adeant <35200053+adeant@users.noreply.github.com>
2018-12-16 19:49:51 -0600

    reading progress

---
+++ AtomPython/MathPython/workingWithFiles.py
@@ -0,0 +1,8 @@
+import os
+# os.getcwd()
+# os.chdir('C:\\Windows\System32)
+# os.path.getsize(path) - size in bytes of path
+# os.listdir(path) - will return a list of filename strings for each file in path
+# os.path.exists(path) - will return True if a file or folder exists
+# os.path.isdir(path) - returns true if path is a folder
+# os.path.isfile(path) - returns true if pathe is a file
