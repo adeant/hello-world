dd788dfad79c14b40162bd806705b0c4169b14c7
adeant <35200053+adeant@users.noreply.github.com>
2018-12-30 20:02:46 -0600

    home sale updates

--- AtomPython/HomeSalesAnalysis/scratch.py
+++ AtomPython/HomeSalesAnalysis/scratch.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
-chromeDriver = webdriver.Chrome('C:\\Users\\Alex Trim\\Documents\\GitHub\\hello-world\\AtomPython\\Miscellaneous\\chromedriver.exe')
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+# chromeDriver = webdriver.Chrome('C:\\Users\\Alex Trim\\Documents\\GitHub\\hello-world\\AtomPython\\Miscellaneous\\chromedriver.exe')
+chromeDriver = webdriver.Chrome('C:\\Users\\Dean\\Documents\\GitHub\\hello-world\\AtomPython\\Miscellaneous\\chromedriver.exe')
 chromeDriver.get('https://www.cityofmadison.com/assessor/property/salesbyarea.cfm')
 areaElem = chromeDriver.find_element_by_id('AssessmentArea')
-from selenium.webdriver.support.ui import Select
 # select the field
 areaSelect = Select(areaElem)
 # get all of the available options
@@ -15,8 +17,9 @@ submitBtn = chromeDriver.find_element_by_css_selector("input[type='submit']")
 submitBtn.click()
 # grab the page source
 pageSource = chromeDriver.page_source
-# get the demystifier
-from bs4 import BeautifulSoup
 # soup the page
 pageSoup = BeautifulSoup(pageSource)
-pageSoup.find_all('tbody')
+tbodies = pageSoup.find_all('tbody')
+pageSoup.find('tbody')
+for cell in tbodies[0].find_all('td'): print(cell.text.strip())
+for option in areaOptions: print(option.text)
