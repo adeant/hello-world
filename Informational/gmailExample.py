6dc2792bfd5e87e196ce944e2c47c55125f229f7
adeant <35200053+adeant@users.noreply.github.com>
2019-03-11 21:30:42 -0500

    updates to gmail testing

--- Informational/gmailExample.py
+++ Informational/gmailExample.py
@@ -19,11 +19,56 @@ from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
+from googleapiclient import errors
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from email.mime.text import MIMEText
+import base64
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send'
+    ]
+
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+    Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+    Returns:
+    An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+def send_message(service, user_id, message):
+    """Send an email message.
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+    Returns:
+      Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message)
+            .execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print ('An error occurred: %s' % error)
+        return None
+
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -62,6 +107,10 @@ def main():
         print('Labels:')
         for label in labels:
             print(label['name'])
+
+    print('testing...')
+    message = create_message('me', 'me', 'test', 'test text')
+    send_message(service, 'me', message)
 
 if __name__ == '__main__':
     main()
